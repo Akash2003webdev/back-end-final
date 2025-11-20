1211c8abbb39db95d82bd39f43f00519bc2f99c4
Akash2003webdev <hariaksh418@gmail.com>
2025-11-20 22:12:03 +0530

    edited

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const app = require("./app");
+import app from "./app.js";
+
 const connectDB = require("./src/config/db");
 
 connectDB();
@@ -8,3 +9,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+export default app;
